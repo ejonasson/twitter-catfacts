56f00cc88a396e87328c8c0478ddcb167e20af02
Erik Jonasson <ejonasson@gmail.com>
2016-07-06 19:33:49 -0400

    wrap extension functionality in its own object

--- content.js
+++ content.js
@@ -37,23 +37,32 @@ class TweetProcessor
 
 }
 
-getStoredTwitterUser();
+(function () {
+    "use strict";
 
-// Get the User from storage
-// Default is realDonaldTrump
-function getStoredTwitterUser() {
-    chrome.storage.sync.get('twitterUser', function(items) {
-        let twitterUser = items.twitterUser;
-        if (twitterUser == '') {
-            twitterUser = 'realDonaldTrump';
+    let TwitterCatfacts = {
+        init () {
+            this.prepareTwitterUser();
+        },
+        
+        prepareTwitterUser () {
+            let self = this;
+            chrome.storage.sync.get('twitterUser', function(items) {
+                let twitterUser = items.twitterUser;
+                if (twitterUser == '') {
+                    twitterUser = 'realDonaldTrump';
+                }
+                self.setupTweetProcessor(twitterUser);
+            });
+        },
+
+        setupTweetProcessor (twitterUser) {
+            window.setInterval(function() {
+            let twitterElements = TweetProcessor.getTweetsByData('data-screen-name', twitterUser);
+            TweetProcessor.loadCatFacts(twitterElements);
+            }, 500);
         }
-        setupTweetProcessor(twitterUser);
-    });
-}
+    }
 
-function setupTweetProcessor(twitterUser) {
-    window.setInterval(function() {
-        let twitterElements = TweetProcessor.getTweetsByData('data-screen-name', twitterUser);
-        TweetProcessor.loadCatFacts(twitterElements);
-    }, 500);
-}
+    TwitterCatfacts.init();
+})();
--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
 import TweetProcessor from './TweetProcessor.js';
+(function () {
+    "use strict";
 
-getStoredTwitterUser();
+    let TwitterCatfacts = {
+        init () {
+            this.prepareTwitterUser();
+        },
+        
+        prepareTwitterUser () {
+            let self = this;
+            chrome.storage.sync.get('twitterUser', function(items) {
+                let twitterUser = items.twitterUser;
+                if (twitterUser == '') {
+                    twitterUser = 'realDonaldTrump';
+                }
+                self.setupTweetProcessor(twitterUser);
+            });
+        },
 
-// Get the User from storage
-// Default is realDonaldTrump
-function getStoredTwitterUser() {
-    chrome.storage.sync.get('twitterUser', function(items) {
-        let twitterUser = items.twitterUser;
-        if (twitterUser == '') {
-            twitterUser = 'realDonaldTrump';
+        setupTweetProcessor (twitterUser) {
+            window.setInterval(function() {
+            let twitterElements = TweetProcessor.getTweetsByData('data-screen-name', twitterUser);
+            TweetProcessor.loadCatFacts(twitterElements);
+            }, 500);
         }
-        setupTweetProcessor(twitterUser);
-    });
-}
+    }
 
-function setupTweetProcessor(twitterUser) {
-    window.setInterval(function() {
-        let twitterElements = TweetProcessor.getTweetsByData('data-screen-name', twitterUser);
-        TweetProcessor.loadCatFacts(twitterElements);
-    }, 500);
-}
+    TwitterCatfacts.init();
+})();
+
